bac6142ef56f2657aa46bd171ca46953473d35c6
Omri Bar-Zik <omri@bar-zik.com>
2016-05-03 16:40:44 +0300

    testing sound and delay
    in the file watekey i expiremaented in delay and display sound.

--- BIN/watekey.asm
+++ BIN/watekey.asm
@@ -13,68 +13,82 @@ x 				dw 160
 y				dw 100
 color			db 1
 TogelRectangle	db 0
+note 			dw ?
+Clock 			equ es:6Ch
 ; --------------------------
 CODESEG
 
 proc key_test
 	cmp ah, 1Eh ;left
 	jne right
-	;mov dx, offset messageleft
-	;mov ah, 9h
-	;int 21h
 	mov cx,5
 	loopleft:
 	sub [x], 1h
 	call PaintDot
 	loop loopleft
+	call closespekers
+	call openspekers
+	mov [note], 2394h
+	call sunduotput
+	call delay
 	ret
 right:
 	cmp ah, 20h ;right
 	jne up
-	;mov dx, offset messageright
-	;mov ah, 9h
-	;int 21h
 	mov cx,5
 	loopright:
 	add [x], 1h
 	call PaintDot
 	loop loopright
+	call closespekers
+	call openspekers
+	mov [note], 2420h
+	call sunduotput
+	call delay
 	ret
 up:
 	cmp ah, 11h ;up
 	jne down
-	;mov dx, offset messageup
-	;mov ah, 9h
-	;int 21h
 	mov cx,5
 	loopup:
 	sub [y], 1h
 	call PaintDot
 	loop loopup
+	call closespekers
+	call openspekers
+	mov [note], 2711h
+	call sunduotput
+	call delay
 	ret
 down:
 	cmp ah, 1Fh ;down
 	jne escp
-	;mov dx, offset messagedown
-	;mov ah, 9h
-	;int 21h
 	mov cx,5
 	loopdown:
 	add [y], 1h
 	call PaintDot
 	loop loopdown
+	call openspekers
+	mov [note], 3043h
+	call sunduotput
+	call delay
 	ret
 escp:
 	cmp ah, 1h 	;esc
 	jne space
+	call closespekers
 	mov ah, 0
 	mov al, 2
 	int 10h
 	mov ah, 4Ch
-	int 21h
+	int 21h	
 	ret
 space:
-	or [TogelRectangle]
+	cmp ah, 39h
+	jne key_test_exit
+	call closespekers
+key_test_exit:
+	ret
 endp key_test
 
 proc PaintDot
@@ -96,6 +110,53 @@ proc PaintDot
 	ret
 endp PaintDot
 
+proc delay
+	mov ax, 40h
+	mov es, ax
+	mov ax, [Clock]
+	FirstTick:
+	cmp ax, [Clock]
+	je FirstTick
+	; count 10 sec
+	mov cx, 2 ;10x0.055sec = ~0.5sec
+	DelayLoop:
+	mov ax, [Clock]
+	Tick:
+	cmp ax, [Clock]
+	je Tick
+	loop DelayLoop
+	call closespekers
+	ret
+endp delay
+
+
+proc openspekers
+	in al, 61h
+	or al, 00000011b
+	out 61h, al
+; --------------------------
+	mov al, 0B6h
+	out 43h, al
+	ret
+endp openspekers
+
+;**********************
+;**********************
+
+proc closespekers
+	in al, 61h
+	and al, 11111100b
+	out 61h, al
+	ret
+endp closespekers
+
+proc sunduotput
+	mov al, 98h
+	out 42h, al ; Sending lower byte
+	mov al, 0Ah
+	out 42h, al 
+	ret	
+endp sunduotput
 
 start:
 	mov ax, @data
@@ -105,6 +166,7 @@ start:
 	mov ax, 13h
 	int 10h
 ; --------------------------
+	;call openspekers
 	mov dx, offset messagestart
 	mov ah, 9h
 	int 21h
@@ -116,11 +178,7 @@ start:
 	mov ah, 0 ; there is a key in the buffer, read it and clear the buffer
 	int 16h
 	jmp WaitForData
-	
-	
-	
 ; --------------------------
-	
 exit:
 	mov ax, 4c00h
 	int 21h
